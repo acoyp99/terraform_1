54a6f11823a15870245a06ca8c03039e5411b604
acoyp99 <61330297+acoyp99@users.noreply.github.com>
2020-03-26 10:27:42 -0500

    Update and rename vsi.tf to bare_metal.tf

---
+++ bare_metal.tf
@@ -0,0 +1,13 @@
+resource "ibm_compute_bare_metal" "hourly-bm1" {
+  hostname                   = "${var.bm_hostname}"
+  domain                     = "${var.bm.domain}"
+  os_reference_code          = "${var.bm_os_reference_code}"
+  datacenter                 = "${var.datacenter}"
+  network_speed              = ${var.vm_network_speed}
+  hourly_billing             = "${var.hourly_billing}"
+  private_network_only       = "${var.hourly_billing}"
+  cores                      = ${var.cores}
+  memory                     = ${var.bm_memory}
+  disks                      = [${var.bm_disks}]
+  local_disk                 = false
+}

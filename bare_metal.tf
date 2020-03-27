ab40fd761df746e3233382f9d472100b0c360253
acoyp99 <61330297+acoyp99@users.noreply.github.com>
2020-03-27 09:36:19 -0500

    Update bare_metal.tf

--- bare_metal.tf
+++ bare_metal.tf
@@ -3,7 +3,7 @@ resource "ibm_compute_bare_metal" "baremetal01" {
   domain                     = "${var.bm_domain}"
   os_reference_code          = "${var.bm_os_reference_code}"
   datacenter                 = "${var.datacenter}"
-  network_speed              = "${var.vm_network_speed}"
+  network_speed              = "${var.bm_network_speed}"
   hourly_billing             = "${var.hourly_billing}"
   private_network_only       = "${var.private_network}"
   fixed_config_preset        = "S1270_8GB_2X1TBSATA_NORAID"

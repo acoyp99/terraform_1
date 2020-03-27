d12458e4d07169a0793f653088bf59edba2c6bf2
acoyp99 <61330297+acoyp99@users.noreply.github.com>
2020-03-27 10:01:19 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
-provider ibm {
-  ibmcloud_api_key      = "${var.ibmcloud_apikey}"
-  region                = "${var.ibm_region}"
-  generation            = 1
-  ibmcloud_timeout      = 60
-}
+#provider ibm {
+#  ibmcloud_api_key      = "${var.ibmcloud_apikey}"
+#  region                = "${var.ibm_region}"
+#  generation            = 1
+#  ibmcloud_timeout      = 60
+#}
 
 ##############################################################################
 
@@ -12,6 +12,6 @@ provider ibm {
 # Resource Group
 ##############################################################################
 
-data ibm_resource_group resource_group {
-  name = "${var.resource_group}"
-}
+#data ibm_resource_group resource_group {
+#  name = "${var.resource_group}"
+#}

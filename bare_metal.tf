38bafd923aa9ac06d737c9d2df2e2755d773a041
acoyp99 <61330297+acoyp99@users.noreply.github.com>
2020-03-30 16:34:40 -0500

    Update bare_metal.tf

--- bare_metal.tf
+++ bare_metal.tf
@@ -6,6 +6,6 @@ resource "ibm_compute_bare_metal" "baremetal01" {
   network_speed              = "${var.bm_network_speed}"
   hourly_billing             = "${var.hourly_billing}"
   private_network_only       = "${var.private_network}"
-  fixed_config_preset        = "${var.processor}"
+  package_key_name           = "${var.processor}"
   notes                      = "${var.notes_bm}"
 }

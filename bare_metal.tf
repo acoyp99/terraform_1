73d2b501161da39441c62788ac2c70519e06e611
acoyp99 <61330297+acoyp99@users.noreply.github.com>
2020-03-31 11:23:57 -0500

    Update bare_metal.tf

--- bare_metal.tf
+++ bare_metal.tf
@@ -1,6 +1,8 @@
 resource "ibm_compute_bare_metal" "baremetal01" {
+  
   hostname                   = "${var.bm_hostname}"
   domain                     = "${var.bm_domain}"
+  memory                     =  32
   os_key_name                = "${var.bm_os_reference_code}"
   datacenter                 = "${var.datacenter}"
   network_speed              = "${var.bm_network_speed}"
@@ -9,4 +11,5 @@ resource "ibm_compute_bare_metal" "baremetal01" {
   package_key_name           = "${var.processor}"
   process_key_name           = "${var.key_process}"
   notes                      = "${var.notes_bm}"
-}
+    
+  }

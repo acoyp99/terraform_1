76e923fec01d86ec671278b79d336e435770f491
acoyp99 <61330297+acoyp99@users.noreply.github.com>
2020-07-13 23:50:30 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider ibm {
-  ibmcloud_api_key      = "HU5KaMf34arRT8c9J8yJqlcK4PiUfT-wMMZfS7ZZ4O_3"
+  ibmcloud_api_key      = ""
   region                = "us-south"
   generation            = 1
   ibmcloud_timeout      = 60
